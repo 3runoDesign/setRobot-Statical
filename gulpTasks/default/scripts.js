3f5225b3753160afbae881d3a701902dc9738f7a
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2016-09-30 02:20:27 -0300

    Fix code style (scss)

--- gulpTasks/default/scripts.js
+++ gulpTasks/default/scripts.js
@@ -2,16 +2,9 @@
 
 var concat   = require('gulp-concat');
 var connect  = require('gulp-connect');
-var eslint   = require('gulp-eslint');
 var gulp     = require('gulp');
-
 var path     = require('../paths.js');
 
-gulp.task('eslint', function() {
-  return gulp.src(path.to.scripts.source)
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
 
 gulp.task('scripts', ['eslint'], function() {
   return gulp.src(path.to.scripts.source)
